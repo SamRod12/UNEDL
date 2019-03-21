feace90179376a952bd0ba0e05851ded910e1076
leo <rsnav.81@gmail.com>
2019-03-20 22:13:14 -0600

    Console

--- Console/TPVIII_T2/TPVIII_T2/Program.cs
+++ Console/TPVIII_T2/TPVIII_T2/Program.cs
@@ -10,6 +10,19 @@ namespace TPVIII_T2
     {
         static void Main(string[] args)
         {
+            int min = char.MinValue, max = char.MaxValue;
+           
+            Console.WriteLine("valores de un byte: [" + byte.MinValue + " , " + byte.MaxValue + "]");
+            Console.WriteLine("valores de un short: [" + short.MinValue + " , " + short.MaxValue + "]");
+            Console.WriteLine("valores de un int: ["+int.MinValue +" , "+int.MaxValue +"]");
+            Console.WriteLine("valores de un long: [" + long.MinValue + " , " + long.MaxValue + "]");
+            Console.WriteLine("valores de un float: [" + float.MinValue + " , " + float.MaxValue + "]");
+            Console.WriteLine("valores de un double: [" + double.MinValue + " , " + double.MaxValue + "]");
+            Console.WriteLine("valores de un decimal: [" + decimal.MinValue + " , " + decimal.MaxValue + "]");
+            Console.WriteLine("valores de un char: [" + min + " , " + max + "]");
+            Console.WriteLine("valores de un boolean: [" + bool.FalseString + " , " + bool.TrueString + "]");
+
+
         }
     }
 }
