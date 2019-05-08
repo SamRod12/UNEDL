38ccd7b631f3d53531c7ebb52de0578a1194fe53
leo <rsnav.81@gmail.com>
2019-05-07 23:08:48 -0500

    tarea 1 parcial 2

---
+++ Console/PVII_T1_Par2/delegate/delegate/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    public delegate void valores(Double x, Double y);
+
+    class delagate {
+        static void Main(String[] artgs) {
+            
+            Double x, y;
+            
+            x = Convert.ToSingle(Console.ReadLine());
+            y = Convert.ToSingle(Console.ReadLine());
+            valores val = new valores(operaciones.suma);
+            val(x,y);
+            val = new valores(operaciones.res);
+            val(x, y);
+            val = new valores(operaciones.mul);
+            val(x, y);
+            val = new valores(operaciones.div);
+            val(x, y);
+        }
+
+
+
+        class  operaciones{
+            public static void suma(Double x, Double y) {
+
+               Console.WriteLine("resultado de suma es:   "+ (x + y));
+            }
+            public static void res(Double x, Double y)
+            {
+            
+                Console.WriteLine("resultado de la resta es:   " + (x - y));
+            }
+            public static void mul(Double x, Double y)
+            {
+
+                Console.WriteLine("resultado de multipiplicacion es:    " + (x * y));
+            }
+            public static void div(Double x, Double y)
+            { 
+                Console.WriteLine("resultado de la division es:      " + (x / y));
+            }
+
+        }
+
+    }
+
+
+
+
+    }
