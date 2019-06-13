77a8316c59ffe6f5750d42219f3ee24ad56c1858
leo <rsnav.81@gmail.com>
2019-06-13 09:11:47 -0500

    exception

---
+++ Console/PersonalizedException/PersonalizedException/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalizedException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception e = new Exception();
+            
+        }
+    }
+}
