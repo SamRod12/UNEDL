cc8ee4867e9a59366b0a64fdf9a1b2fc9aebb28f
leo <rsnav.81@gmail.com>
2019-06-21 10:23:31 -0500

    rotacion de arreglo finalizado

--- Rotacion/Rotacion.java
+++ Rotacion/Rotacion.java
@@ -13,31 +13,38 @@ import javax.swing.JOptionPane;
  */
 public class Rotacion {
     private static int[] arr ;
+ 
     public static void main(String args[]){
+        Rotacion rota = new Rotacion();
         int tamaño;
         tamaño = Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese el tamaño del arreglo"));
          arr = new int[tamaño];
         for(int x = 0; x<arr.length;x++){
             arr[x] = Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor en la posicion : "+ x));
         }
-        int rot = Integer.parseInt(JOptionPane.showInputDialog("cuantas veces quiere rotar a la izquierda el arreglo?")); 
-        imprimir();
-        rotarIzda();
-       
-        imprimir();
+       int  rot = Integer.parseInt(JOptionPane.showInputDialog("cuantas veces quiere rotar a la izquierda el arreglo?")); 
+        rota.imprimir();
+       rota.rotarIzda(rot);
+       rota.imprimir();
     }
     
-  private static void rotarIzda(){
-        int aux = arr[0];
-        for (int i = 0; i < arr.length-1; i++) {
+  void rotarIzda(int pos){
+        for(int i=1;i<=pos;i++){
+            rotarIzda1Pos();
+        }
+    }
+     
+    void rotarIzda1Pos(){
+        int temp = arr[0];
+        for(int i=0;i<arr.length-1;i++){
             arr[i] = arr[i+1];
         }
-        arr[arr.length-1] = aux;
+        arr[arr.length-1] = temp;
     }
-  private static void imprimir(){
-      for(int x =0;x<arr.length;x++){
-          System.out.print(arr[x]);
-      }
-      System.out.println();
-  }
+    void imprimir(){
+        for(int x = 0;x<arr.length;x++){
+            System.out.print(arr[x]);
+        }
+        System.out.println();
+    }
 }
